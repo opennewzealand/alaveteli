dd167f39a27fcc7de748ae909c2e66931f085e06
Rowan Crawford <wombleton@gmail.com>
2013-05-01 15:47:04 +1200

    add theme loader

---
+++ config/initializers/theme_loader.rb
@@ -0,0 +1,12 @@
+# This is a global array of route extensions. Alaveteli modules may add to it.
+# It is used by our config/routes.rb to decide which route extension files to load.
+$alaveteli_route_extensions = []
+
+if ENV["RAILS_ENV"] != "test" # Don't let the themes interfere with Alaveteli specs
+    url = MySociety::Config.get('THEME_URL')
+    theme_name = url.sub(/.*\/(.*).git/, "\\1")
+    theme_main_include = File.expand_path "../../../vendor/plugins/#{theme_name}/lib/alavetelitheme.rb", __FILE__
+    if File.exists? theme_main_include
+        require theme_main_include
+    end
+end
